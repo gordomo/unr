ea43cda0e51b860fd753c75086c7d33d86a18f61
mmori <martin.mori@martindale.com>
2018-02-16 13:54:17 -0300

    dev-fix - db initial stage

--- assets/datatables/script_inicial.sql
+++ assets/datatables/script_inicial.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 23-01-2018 a las 13:57:15
+-- Tiempo de generación: 16-02-2018 a las 16:52:13
 -- Versión del servidor: 5.7.14
 -- Versión de PHP: 5.6.25
 
@@ -31,7 +31,9 @@ CREATE TABLE `apuntes` (
   `name` text NOT NULL,
   `cat_id` int(11) NOT NULL,
   `sub_cat_id` int(11) NOT NULL,
-  `file` text NOT NULL
+  `file` text NOT NULL,
+  `pages` int(11) NOT NULL,
+  `subsub_cat_id` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -49,6 +51,68 @@ CREATE TABLE `categorias` (
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `configuracion`
+--
+
+CREATE TABLE `configuracion` (
+  `id` int(11) NOT NULL,
+  `price_pages` double DEFAULT NULL,
+  `ringed` double DEFAULT NULL,
+  `double_fas` double DEFAULT NULL,
+  `demora` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `historial`
+--
+
+CREATE TABLE `historial` (
+  `id` int(8) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `admin` text NOT NULL,
+  `mov` text NOT NULL,
+  `amount` double NOT NULL,
+  `date` datetime NOT NULL,
+  `estado` int(11) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `id_pedido` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pedidos`
+--
+
+CREATE TABLE `pedidos` (
+  `id` int(11) NOT NULL,
+  `nombre` text NOT NULL,
+  `archivo` text NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `total` double NOT NULL,
+  `estado` int(11) NOT NULL DEFAULT '1',
+  `date` datetime NOT NULL,
+  `usr_id` int(11) NOT NULL,
+  `anillado` int(1) NOT NULL,
+  `doblefaz` int(1) NOT NULL DEFAULT '0'
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `saldos`
+--
+
+CREATE TABLE `saldos` (
+  `id_usuario` int(8) NOT NULL,
+  `saldo` double NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `subcategorias`
 --
 
@@ -56,6 +120,19 @@ CREATE TABLE `subcategorias` (
   `id` int(8) NOT NULL,
   `name` text NOT NULL,
   `cat_id` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `subsubcategorias`
+--
+
+CREATE TABLE `subsubcategorias` (
+  `id` int(11) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `cat_id` int(11) NOT NULL,
+  `sub_cat_id` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -90,9 +167,39 @@ ALTER TABLE `categorias`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indices de la tabla `configuracion`
+--
+ALTER TABLE `configuracion`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `historial`
+--
+ALTER TABLE `historial`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `pedidos`
+--
+ALTER TABLE `pedidos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `saldos`
+--
+ALTER TABLE `saldos`
+  ADD PRIMARY KEY (`id_usuario`);
+
+--
 -- Indices de la tabla `subcategorias`
 --
 ALTER TABLE `subcategorias`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `subsubcategorias`
+--
+ALTER TABLE `subsubcategorias`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -109,22 +216,37 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `apuntes`
 --
 ALTER TABLE `apuntes`
-  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 --
 -- AUTO_INCREMENT de la tabla `categorias`
 --
 ALTER TABLE `categorias`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+--
+-- AUTO_INCREMENT de la tabla `historial`
+--
+ALTER TABLE `historial`
+  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+--
+-- AUTO_INCREMENT de la tabla `pedidos`
+--
+ALTER TABLE `pedidos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
 --
 -- AUTO_INCREMENT de la tabla `subcategorias`
 --
 ALTER TABLE `subcategorias`
-  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT de la tabla `subsubcategorias`
+--
+ALTER TABLE `subsubcategorias`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
